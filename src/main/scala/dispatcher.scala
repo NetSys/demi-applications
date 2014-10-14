7475f330150da7c089a9ca8e890c07ba5326f7b1
Aurojit <apanda@cs.berkeley.edu>
2014-10-14 14:40:58 -0700

    Better inormation at the dispatcher

--- src/main/scala/dispatcher.scala
+++ src/main/scala/dispatcher.scala
@@ -22,7 +22,7 @@ class InstrumentedDispatcher(
   extends Dispatcher(_configurator, _id, throughput, throughputDeadlineTime, _executorServiceFactoryProvider,
 _shutdownTimeout) {
   protected[akka] override def dispatch(receiver: ActorCell, invocation: Envelope): Unit = {
-    println("Dispatch called for " + receiver)
+    println("Dispatch called for " + receiver.actor.self.path.name + " " + invocation)
     super.dispatch(receiver, invocation)
     println("Done dispatching")
   }
