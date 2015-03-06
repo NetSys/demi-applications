f0df80ec2c4915100c767cde4ce57c1d1b7b20d4
Aurojit <apanda@cs.berkeley.edu>
2015-03-05 17:09:24 -0800

    Add ordering heuristic to DPOR

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -67,10 +67,10 @@ object Test extends App {
     Array[ExternalEvent](
     Send("bcast5", () => Bcast(null, Msg("Foo", 1))),
     Send("bcast5", () => Bcast(null, Msg("Foo", 2))),
-    Send("bcast8", () => Bcast(null, Msg("Bar", 2))),
-    Unique(WaitQuiescence),
-    Send("bcast5", () => Bcast(null, Msg("Foo", 1))),
-    Send("bcast8", () => Bcast(null, Msg("Bar", 2)))
+    Send("bcast8", () => Bcast(null, Msg("Bar", 2)))//,
+    //Unique(WaitQuiescence),
+    //Send("bcast5", () => Bcast(null, Msg("Foo", 1))),
+    //Send("bcast8", () => Bcast(null, Msg("Bar", 2)))
   )
 
   val sched = new DPORwHeuristics
