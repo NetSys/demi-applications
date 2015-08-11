61780ec42732974d2eda31eb9b5679cd690ad61a
colin-scott <cs@cs.berkeley.edu>
2015-08-10 21:38:37 -0700

    Move indent to tools/

--- tools/indent.rb
+++ tools/indent.rb
@@ -1,18 +1,12 @@
 #!/usr/bin/ruby
 
-once = false
 File.foreach(ARGV.shift) do |line|
   if line =~ /BEFORE/
     puts "  " + line
   elsif line =~ /AFTER/
     puts "  " + line
-    if (once)
-      once = false
-      puts
-    else
-      once = true
-    end
   elsif line =~ /schedule_new_message/
+    puts
     puts line
     puts
   end
