21f104a00a857ddd469f855ab30582d81fdf9c03
colin-scott <cs@cs.berkeley.edu>
2015-09-02 16:26:04 -0700

    Paranoid=true

--- src/main/scala/pl/project13/Runner.scala
+++ src/main/scala/pl/project13/Runner.scala
@@ -266,6 +266,6 @@ object Main extends App {
       })
 
     RunnerUtils.runTheGamut(dir, mcs_dir, schedulerConfig, msgSerializer,
-      msgDeserializer, paranoid=false, shouldRerunDDMin=shouldRerunDDMin)
+      msgDeserializer, paranoid=true, shouldRerunDDMin=shouldRerunDDMin)
   }
 }
