4714450481f60b4cdb5f816c71b78bb4efea9ab8
colin-scott <cs@cs.berkeley.edu>
2015-09-02 13:29:30 -0700

    100 external events for raft-58-init FIFO

--- src/main/scala/pl/project13/Runner.scala
+++ src/main/scala/pl/project13/Runner.scala
@@ -248,7 +248,7 @@ object Main extends App {
   val weights = new FuzzerWeights(kill=0.00, send=0.3, wait_quiescence=0.0,
                                   partition=0.0, unpartition=0)
   val messageGen = new ClientMessageGenerator(members)
-  val fuzzer = new Fuzzer(60, weights, messageGen, prefix, postfix=postfix)
+  val fuzzer = new Fuzzer(100, weights, messageGen, prefix, postfix=postfix)
 
   val fuzz = false
 
@@ -305,136 +305,25 @@ object Main extends App {
     }
 
     val mcs_dir = serializer.serializeMCS(dir, mcs, stats1, verified_mcs, violation, false)
+    println("verified_mcs.size: " +
+      RunnerUtils.getDeliveries(verified_mcs.get).size)
     println("MCS DIR: " + mcs_dir)
   } else { // !fuzz
     val dir =
-    "experiments/akka-raft-fuzz-long_2015_08_28_19_01_08"
+    "experiments/akka-raft-fuzz-long_2015_09_02_13_25_20"
     val mcs_dir =
-    "experiments/akka-raft-fuzz-long_2015_08_28_19_01_08_DDMin_STSSchedNoPeek"
+    "experiments/akka-raft-fuzz-long_2015_09_02_13_25_20_DDMin_STSSchedNoPeek"
 
-    val serializer = new ExperimentSerializer(
-      fingerprintFactory,
-      new RaftMessageSerializer)
-
-    val deserializer = new ExperimentDeserializer(mcs_dir)
+    val msgSerializer = new RaftMessageSerializer
     val msgDeserializer = new RaftMessageDeserializer(Instrumenter()._actorSystem)
 
-    // -- purely for printing stats --
-    val (traceFound, _, _) = RunnerUtils.deserializeExperiment(dir, msgDeserializer)
-    val origDeserializer = new ExperimentDeserializer(dir)
-    val filteredTrace = origDeserializer.get_filtered_initial_trace
-    // -- --
+    def shouldRerunDDMin(externals: Seq[ExternalEvent]) =
+      externals.exists({
+        case s: Send => s.messageCtor.isInstanceOf[AppendWordConstuctor]
+        case _ => false
+      })
 
-    val (mcs, verified_mcs, violationFound, actors, stats) =
-      RunnerUtils.deserializeMCS(mcs_dir, msgDeserializer)
-
-    var currentExternals = mcs
-    var currentTrace = verified_mcs
-    var currentStats = stats
-
-    var removalStrategy = new SrcDstFIFORemoval(currentTrace,
-      schedulerConfig.messageFingerprinter)
-
-    val (intMinStats, intMinTrace) = RunnerUtils.minimizeInternals(schedulerConfig,
-      currentExternals, currentTrace, actors, violationFound,
-      removalStrategyCtor=() => removalStrategy, stats=Some(currentStats))
-
-    currentTrace = intMinTrace
-    currentStats = intMinStats
-
-    var additionalTraces = Seq[(String, EventTrace)]()
-
-    // fungibleClocks DDMin without backtracks.
-    val (newMCS, clocksDDMinStats, clockDDMinTrace, _) =
-      RunnerUtils.fungibleClocksDDMin(schedulerConfig,
-        currentTrace,
-        violationFound,
-        actors,
-        stats=Some(currentStats))
-
-    currentTrace = clockDDMinTrace.get
-    currentStats = clocksDDMinStats
-    currentExternals = newMCS
-
-    additionalTraces = additionalTraces :+ (("WildcardDDMinNoBacktracks", clockDDMinTrace.get))
-
-    // Without backtracks first
-    var minimizer = new FungibleClockMinimizer(schedulerConfig,
-      currentExternals,
-      currentTrace, actors, violationFound,
-      //testScheduler=TestScheduler.DPORwHeuristics,
-      stats=Some(currentStats))
-    val (wildcard_stats1, clusterMinTrace1) = minimizer.minimize
-
-    currentTrace = clusterMinTrace1
-    currentStats = wildcard_stats1
-
-    additionalTraces = additionalTraces :+ (("FungibleClocksNoBackTracks", clusterMinTrace1))
-
-    // N.B. will be overwritten
-    serializer.recordMinimizedInternals(mcs_dir,
-       currentStats, currentTrace)
-
-    RunnerUtils.printMinimizationStats(
-      traceFound, filteredTrace, verified_mcs, intMinTrace, schedulerConfig.messageFingerprinter,
-      additionalTraces)
-
-    // Now with backtracks
-    minimizer = new FungibleClockMinimizer(schedulerConfig, currentExternals,
-      currentTrace, actors, violationFound,
-      testScheduler=TestScheduler.DPORwHeuristics,
-      stats=Some(currentStats))
-    val (wildcard_stats2, clusterMinTrace2) = minimizer.minimize
-
-    currentStats = wildcard_stats2
-    currentTrace = clusterMinTrace2
-
-    additionalTraces = additionalTraces :+ (("FungibleClocks", clusterMinTrace2))
-
-    removalStrategy = new SrcDstFIFORemoval(currentTrace,
-      schedulerConfig.messageFingerprinter)
-
-     val (intMinStats2, minTrace2) = RunnerUtils.minimizeInternals(schedulerConfig,
-       currentExternals, currentTrace, actors, violationFound,
-       removalStrategyCtor=() => removalStrategy,
-       stats=Some(currentStats))
-
-     currentStats = intMinStats2
-     currentTrace = minTrace2
-
-     additionalTraces = additionalTraces :+ (("2nd intMin", minTrace2))
-
-    // N.B. overwrite
-    serializer.recordMinimizedInternals(mcs_dir,
-       currentStats, currentTrace)
-
-    RunnerUtils.printMinimizationStats(
-      traceFound, filteredTrace, verified_mcs, intMinTrace, schedulerConfig.messageFingerprinter,
-      additionalTraces)
-
-    /*
-    // fungibleClocks DDMin with backtracks.
-    val (newMCS2, clocksDDMinStats2, clockDDMinTrace2, _) =
-      RunnerUtils.fungibleClocksDDMin(schedulerConfig,
-        clusterMinTrace2,
-        violationFound,
-        actors,
-        testScheduler=TestScheduler.DPORwHeuristics,
-        stats=Some(wildcard_stats2))
-
-    currentExternals= newMCS2
-    currentStats = clocksDDMinStats2
-    currentTrace = clockDDMinTrace2.get
-
-    // N.B. overwrite
-    serializer.recordMinimizedInternals(mcs_dir,
-       clocksDDMinStats2, clockDDMinTrace2.get)
-
-    additionalTraces = additionalTraces :+ (("WildcardDDMinNoBacktracks", clockDDMinTrace2.get))
-
-    RunnerUtils.printMinimizationStats(
-      traceFound, filteredTrace, verified_mcs, intMinTrace, schedulerConfig.messageFingerprinter,
-      additionalTraces)
-    */
+    RunnerUtils.runTheGamut(dir, mcs_dir, schedulerConfig, msgSerializer,
+      msgDeserializer, paranoid=false, shouldRerunDDMin=shouldRerunDDMin)
   }
 }
