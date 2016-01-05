5058648c98216a032c739faf853522a3f9f7c489
colin-scott <cs@cs.berkeley.edu>
2016-01-04 18:41:57 -0800

    Revert "Revert "if term > currentTerm: goto(Follower) with currentTerm=term""
    This reverts commit 8e9f97f17987c614b91256a1a6e7cf48d975099d.
    
    Conflicts:
    	src/main/scala/pl/project13/scala/akka/raft/Candidate.scala

--- src/main/scala/pl/project13/scala/akka/raft/Candidate.scala
+++ src/main/scala/pl/project13/scala/akka/raft/Candidate.scala
@@ -21,7 +21,7 @@ private[raft] trait Candidate {
     case Event(BeginElection, m: ElectionMeta) =>
       if (m.config.members.isEmpty) {
         log.warning("Tried to initialize election with no members...")
-        goto(Follower) using m.forFollower
+        goto(Follower) using m.forFollower()
       } else {
         log.info("Initializing election (among {} nodes) for {}", m.config.members.size, m.currentTerm)
 
@@ -37,6 +37,10 @@ private[raft] trait Candidate {
         }
       }
 
+    case Event(msg: RequestVote, m: ElectionMeta) if msg.term > m.currentTerm =>
+      log.info("Received newer {}. Current term is {}. Revert to follower state.", msg.term, m.currentTerm)
+      goto(Follower) using m.forFollower(msg.term)
+
     case Event(msg: RequestVote, m: ElectionMeta)
       if m.canVoteIn(msg.term, msg.candidateId) =>
       sender ! VoteCandidate(m.currentTerm)
@@ -45,6 +49,10 @@ private[raft] trait Candidate {
     case Event(msg: RequestVote, m: ElectionMeta) =>
       sender ! DeclineCandidate(msg.term)
       stay()
+
+    case Event(VoteCandidate(term), m: ElectionMeta) if term > m.currentTerm =>
+      log.info("Received newer {}. Current term is {}. Revert to follower state.", term, m.currentTerm)
+      goto(Follower) using m.forFollower(term)
 
     case Event(VoteCandidate(term), m: ElectionMeta) =>
       val includingThisVote = m.incVote(voter())
@@ -56,6 +64,10 @@ private[raft] trait Candidate {
         log.info("Received vote by {}; Have {} of {} votes", voter(), includingThisVote.votesReceived, m.config.members.size)
         stay() using includingThisVote
       }
+
+    case Event(DeclineCandidate(term), m: ElectionMeta) if term > m.currentTerm =>
+      log.info("Received newer {}. Current term is {}. Revert to follower state.", term, m.currentTerm)
+      goto(Follower) using m.forFollower(term)
 
     case Event(DeclineCandidate(term), m: ElectionMeta) =>
       log.info("Rejected vote by {}, in term {}", voter(), term)
@@ -70,7 +82,7 @@ private[raft] trait Candidate {
       if (leaderIsAhead) {
         log.info("Reverting to Follower, because got AppendEntries from Leader in {}, but am in {}", append.term, m.currentTerm)
         m.clusterSelf forward append
-        goto(Follower) using m.forFollower
+        goto(Follower) using m.forFollower()
       } else {
         stay()
       }
@@ -84,7 +96,7 @@ private[raft] trait Candidate {
     // would like to start election, but I'm all alone! ;-(
     case Event(ElectionTimeout, m: ElectionMeta) =>
       log.info("Voting timeout, unable to start election, don't know enough nodes (members: {})...", m.config.members.size)
-      goto(Follower) using m.forFollower
+      goto(Follower) using m.forFollower()
 
     case Event(AskForState, _) =>
       sender() ! IAmInState(Candidate)
--- src/main/scala/pl/project13/scala/akka/raft/protocol/StateMetadata.scala
+++ src/main/scala/pl/project13/scala/akka/raft/protocol/StateMetadata.scala
@@ -56,6 +56,8 @@ private[protocol] trait StateMetadata extends Serializable {
       copy(votes = votes updated (term, candidate))
     }
 
+    def withTerm(term: Term) = copy(currentTerm = term)
+
     def withConfig(conf: ClusterConfiguration): Meta = copy(config = conf)
   }
 
@@ -80,8 +82,8 @@ private[protocol] trait StateMetadata extends Serializable {
     def withVoteFor(term: Term, candidate: ActorRef) = copy(votes = votes + (term -> candidate))
 
     def forLeader: LeaderMeta        = LeaderMeta(clusterSelf, currentTerm, config)
-    def forFollower: Meta            = Meta(clusterSelf, currentTerm, config, Map.empty)
-    def forNewElection: ElectionMeta = this.forFollower.forNewElection
+    def forFollower(term:Term=currentTerm): Meta = Meta(clusterSelf, term, config, votes)
+    def forNewElection: ElectionMeta = this.forFollower().forNewElection
   }
 
   case class LeaderMeta(
@@ -94,8 +96,7 @@ private[protocol] trait StateMetadata extends Serializable {
 
     // todo duplication; yeah, having 3 meta classes was a bad idea. todo make one Meta class
     def withConfig(conf: ClusterConfiguration): LeaderMeta = copy(config = conf)
-
-    def forFollower: Meta = Meta(clusterSelf, currentTerm, config, Map.empty)
+    def forFollower(term:Term=currentTerm): Meta = Meta(clusterSelf, term, config, Map.empty)
   }
 
 }
