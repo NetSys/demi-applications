32ece977d186501c2f1330f1426bcb45281baf93
colin-scott <cs@cs.berkeley.edu>
2016-01-09 16:56:06 -0800

    For efficiency: forward RequestVote when updating Term

--- src/main/scala/pl/project13/scala/akka/raft/Follower.scala
+++ src/main/scala/pl/project13/scala/akka/raft/Follower.scala
@@ -20,9 +20,10 @@ private[raft] trait Follower {
       stay()
 
     // election
-    case Event(RequestVote(term, candidate, lastLogTerm, lastLogIndex), m: Meta)
+    case Event(msg @ RequestVote(term, candidate, lastLogTerm, lastLogIndex), m: Meta)
       if term > m.currentTerm =>
       log.info("Received newer {}. Current term is {}. Revert to follower state.", term, m.currentTerm)
+      m.clusterSelf forward msg
       stay() using m.withTerm(term)
 
     case Event(RequestVote(term, candidate, lastLogTerm, lastLogIndex), m: Meta)
